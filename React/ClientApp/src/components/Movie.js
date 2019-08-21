849d4e85533719687c3a5c2084db98b081f58236
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-08-21 11:20:20 -0700

    template starting point

---
+++ React/ClientApp/src/components/Movie.js
@@ -0,0 +1,31 @@
+﻿import React, { Component } from 'react';
+
+export class Movie extends Component {
+  static displayName = Movie.name;
+
+  constructor (props) {
+    super(props);
+    this.state = { currentCount: 0 };
+    this.incrementCounter = this.incrementCounter.bind(this);
+  }
+
+  incrementCounter () {
+    this.setState({
+      currentCount: this.state.currentCount + 1
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Counter: Movie</h1>
+
+        <p>This is a simple example of a React component.</p>
+
+        <p>Current count: <strong>{this.state.currentCount}</strong></p>
+
+        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+      </div>
+    );
+  }
+}
